023692cf1bca7dd011304e0b9e96c3b04b7c770e
kolodinskiy <kolodinskiy@rit.va>
2021-11-19 10:34:24 +0300

    remove amd store

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -8,7 +8,4 @@ INSERT INTO domain (name, url) VALUES ('lamoda', 'https://www.lamoda.by');
 INSERT INTO catalog_url (id, domain_fk, url) VALUES (3, 'lamoda', '/c/5971/shoes-muzhkrossovki/');
 
 INSERT INTO domain (name, url) VALUES ('oz', 'https://oz.by');
-INSERT INTO catalog_url (domain_fk, url, last_page) VALUES ('oz', '/books/bestsellers', 37);
-
-INSERT INTO domain (name, url) VALUES ('amd', 'https://www.amd.by/catalog');
-INSERT INTO catalog_url (domain_fk, url, last_page) VALUES ('amd', '/krupnogabaritnaya-bytovaya-texnika/stiralnye-mashiny/', 59);
+INSERT INTO catalog_url (id, domain_fk, url) VALUES (4, 'oz', '/books/bestsellers');
